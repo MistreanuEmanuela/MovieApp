34eee1336c7a131f894ec62716cd8b3ee02a0b52
MistreanuEmanela <mistreanu.emanuela@gmail.com>
2024-05-24 23:17:10 +0300

    actor ui update

--- lib/pages/actor_page.dart
+++ lib/pages/actor_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/actor.dart';
 import '../helpers/database_helper.dart';
 import '../models/movie.dart';
-import "../user_preferinces.dart";
-import "../pages/movie_page.dart";
+import '../user_preferinces.dart'; // Fixed import path
+import '../pages/movie_page.dart';
 
 class ActorPage extends StatefulWidget {
   final int actorId;
@@ -47,7 +47,8 @@ class _ActorPageState extends State<ActorPage> {
   }
 
   void _scrollListener() {
-    if (_scrollController.offset >= _scrollController.position.maxScrollExtent && !_scrollController.position.outOfRange) {
+    if (_scrollController.offset >= _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
       setState(() {
         showBibliography = false;
       });
@@ -93,39 +94,32 @@ class _ActorPageState extends State<ActorPage> {
                       children: [
                         Align(
                           alignment: Alignment.topRight,
-                          child: isFavorite
-                              ? IconButton(
-                                  icon: Icon(
+                          child: IconButton(
+                            icon: isFavorite
+                                ? Icon(
                                     Icons.favorite,
                                     color: const Color.fromARGB(255, 255, 0, 0),
                                     size: 40,
-                                  ),
-                                  onPressed: () async {
-                                    await DatabaseHelper().deleteFavoriteMovie(widget.actorId, userId);
-                                    setState(() {
-                                      isFavorite = false;
-                                    });
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(content: Text('Movie deleted')),
-                                    );
-                                  },
-                                )
-                              : IconButton(
-                                  icon: Icon(
+                                  )
+                                : Icon(
                                     Icons.favorite_border_outlined,
                                     color: Colors.white,
                                     size: 40,
                                   ),
-                                  onPressed: () async {
-                                    await DatabaseHelper().insertFavoriteMovie(widget.actorId, userId);
-                                    setState(() {
-                                      isFavorite = true;
-                                    });
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(content: Text('Movie favorited')),
-                                    );
-                                  },
-                                ),
+                            onPressed: () async {
+                              if (isFavorite) {
+                                await DatabaseHelper().deleteFavoriteActor(widget.actorId, userId);
+                              } else {
+                                await DatabaseHelper().insertFavoriteActor(widget.actorId, userId);
+                              }
+                              setState(() {
+                                isFavorite = !isFavorite;
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text(isFavorite ? 'Actor favorited' : 'Actor removed from favorites')),
+                              );
+                            },
+                          ),
                         ),
                         Center(
                           child: CircleAvatar(
@@ -158,7 +152,7 @@ class _ActorPageState extends State<ActorPage> {
                             Icon(Icons.cake, color: Colors.white, size: 18),
                             SizedBox(width: 4.0),
                             Text(
-                              'Birthdate: ${"22.02.33"}', // Assuming actor.birthdate contains the birthdate information
+                              'Birthdate: ${'22.22.22'}', // Assuming actor.birthdate contains the birthdate information
                               style: TextStyle(fontSize: 16.0, color: Colors.white),
                             ),
                           ],
@@ -170,67 +164,81 @@ class _ActorPageState extends State<ActorPage> {
                         ),
                         SizedBox(height: 8.0),
                         FutureBuilder<List<Movie>>(
-                          future: DatabaseHelper().getMoviesForActor(widget.actorId),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return CircularProgressIndicator();
-                            } else if (snapshot.hasError) {
-                              return Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.white));
-                            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                              return Text('No movies found', style: TextStyle(color: Colors.white));
-                            } else {
-                              final movies = snapshot.data!;
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: movies.map((movie) {
-                                  return MouseRegion(
-                                    cursor: SystemMouseCursors.click,
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => MoviePage(movieId: movie.id!), // Assuming MoviePage is the destination page
-                                          ),
-                                        );
-                                      },
-                                      child: Container(
-                                        color: Colors.transparent,
-                                        child: Padding(
-                                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                                          child: Row(
-                                            children: [
-                                              Text(
-                                                movie.year.toString(),
-                                                style: TextStyle(color: Colors.white),
-                                              ),
-                                              SizedBox(width: 8.0),
-                                              Container(
-                                                width: 8,
-                                                height: 8,
-                                                decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  shape: BoxShape.circle,
-                                                ),
-                                              ),
-                                              SizedBox(width: 8.0),
-                                              Expanded(
-                                                child: Text(
-                                                  movie.title,
-                                                  style: TextStyle(fontSize: 16.0, color: Colors.white),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                              );
-                            }
-                          },
+  future: DatabaseHelper().getMoviesForActor(widget.actorId),
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return Center(child: CircularProgressIndicator());
+    } else if (snapshot.hasError) {
+      return Center(child: Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.white)));
+    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+      return Center(child: Text('No movies found', style: TextStyle(color: Colors.white)));
+    } else {
+      final movies = snapshot.data!;
+      return Container(
+        constraints: BoxConstraints(maxHeight: 270), // Limiting max height to 300
+        child: SingleChildScrollView( // Wrapping with SingleChildScrollView
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MoviePage(movieId: movies[index].id!),
                         ),
+                      );
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              movies[index].year.toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(width: 8.0),
+                            Container(
+                              width: 8,
+                              height: 8,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            SizedBox(width: 8.0),
+                            Expanded(
+                              child: Text(
+                                movies[index].title,
+                                style: TextStyle(fontSize: 16.0, color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (index != movies.length - 1)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Divider(color: Colors.white),
+                    ),
+                ],
+              );
+            },
+          ),
+        ),
+      );
+    }
+  },
+),
+  
                         SizedBox(height: 16.0),
                       ],
                     ),
@@ -239,61 +247,60 @@ class _ActorPageState extends State<ActorPage> {
               },
             ),
           ),
-         AnimatedPositioned(
-  duration: Duration(milliseconds: 300),
-  bottom: showBibliography ? 0 : -_bottomContainerHeight,
-  left: 0,
-  right: 0,
-  height: MediaQuery.of(context).size.height * (showBibliography ? 0.7 : 0.1), // Adjust height based on showBibliography
-  child: GestureDetector(
-    onTap: () {
-      setState(() {
-        showBibliography = !showBibliography;
-      });
-    },
-    child: Container(
-      decoration: BoxDecoration(
-        color: showBibliography ? Colors.white.withOpacity(1.0) :  Colors.white.withOpacity(0.3),
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-      ),
-      padding: EdgeInsets.fromLTRB(16.0, showBibliography ? 16.0 : 0.0, 16.0, showBibliography ? 16.0 : 1.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Icon(showBibliography ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up),
-                iconSize: 32,
-                onPressed: () {
-                  setState(() {
-                    showBibliography = !showBibliography;
-                  });
-                },
-              ),
-             
-            ],
-          ),
-          SizedBox(height: 8.0),
-          Expanded(
-            child: AnimatedOpacity(
-              duration: Duration(milliseconds: 300),
-              opacity: showBibliography ? 1.0 : 0.0,
-              child: SingleChildScrollView( // Add SingleChildScrollView
-                child: Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie, erat ac accumsan convallis, ligula orci iaculis lectus, a semper ante eros elementum enim. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer a urna ut urna feugiat vehicula. Nulla blandit enim quis mauris semper fringilla. Donec ornare imperdiet sodales. In maximus urna eget metus sodales lacinia vel et ante. Curabitur iaculis lectus nec ipsum rutrum congue. Mauris semper sit amet nunc hendrerit ultricies. Duis vitae nisl non nibh feugiat vulputate sit amet vel arcu. Etiam tristique, arcu id fermentum sagittis, felis metus rutrum turpis, a viverra purus purus at massa. Fusce vitae urna metus. Fusce nec justo non dolor posuere dictum vel eget ligula. Quisque ac interdum purus. Praesent suscipit urna sit amet lacus viverra, a ullamcorper ex cursus....',
-                  style: TextStyle(fontSize: 16.0),
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 400),
+            bottom: showBibliography ? 0 : -_bottomContainerHeight,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height * (showBibliography ? 0.7 : 0.065), // Adjust height based on showBibliography
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  showBibliography = !showBibliography;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: showBibliography ? Colors.white.withOpacity(1.0) : Colors.white.withOpacity(0.2),
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                ),
+                padding: EdgeInsets.fromLTRB(16.0, showBibliography ? 0.0 : 0.0, 16.0, showBibliography ? 0.0 : 0.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(showBibliography ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up),
+                          iconSize: 32,
+                          onPressed: () {
+                            setState(() {
+                              showBibliography = !showBibliography;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8.0),
+                    Expanded(
+                      child: AnimatedOpacity(
+                        duration: Duration(milliseconds: 300),
+                        opacity: showBibliography ? 1.0 : 0.0,
+                        child: SingleChildScrollView( // Add SingleChildScrollView
+                          child: Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie, erat ac accumsan convallis, ligula orci iaculis lectus, a semper ante eros elementum enim. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer a urna ut urna feugiat vehicula. Nulla blandit enim quis mauris semper fringilla. Donec ornare imperdiet sodales. In maximus urna eget metus sodales lacinia vel et ante. Curabitur iaculis lectus nec ipsum rutrum congue. Mauris semper sit amet nunc hendrerit ultricies. Duis vitae nisl non nibh feugiat vulputate sit amet vel arcu. Etiam tristique, arcu id fermentum sagittis, felis metus rutrum turpis, a viverra purus purus at massa. Fusce vitae urna metus. Fusce nec justo non dolor posuere dictum vel eget ligula. Quisque ac interdum purus. Praesent suscipit urna sit amet lacus viverra, a ullamcorper ex cursus....',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ],
-      ),
-    ),
-  ),
-),
         ],
       ),
     );
