102ac0b32d26b37a75be464576aa7d456726d932
MistreanuEmanela <mistreanu.emanuela@gmail.com>
2024-05-25 13:36:40 +0300

    search display

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import '../helpers/database_helper.dart';
 import '../models/movie.dart';
 import 'movie_page.dart';
-import '../pages/favorite_movies.dart';
-import '../pages/favorite_actors.dart';
+import '../pages/favorite_items_page.dart';
+
 
 class SearchPage extends StatefulWidget {
   @override
@@ -41,59 +41,68 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 2, 28, 70),
-     leading: IconButton(
+        leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          color: Colors.white, // Set icon color to white
+          color: Colors.white,
           onPressed: () {
-            Navigator.pop(context); // Navigate back to the previous screen
+            Navigator.pop(context);
           },
-    
         ),
         actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            color: Colors.white, // Set icon color to white
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => SearchPage(),
-              //   ),
-              // );
-            },
+    IconButton(
+      icon: Icon(Icons.search),
+      color: Colors.white,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SearchPage(),
           ),
-          IconButton(
-            icon: Icon(Icons.movie),
-            color: Colors.white, // Set icon color to white
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FavoriteMoviesPage(),
-                ),
-              );
-            },
+        );
+      },
+    ),
+    IconButton(
+      icon: Icon(Icons.favorite_outlined),
+      color: Colors.white,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FavoriteItemsPage(),
           ),
-          IconButton(
-            icon: Icon(Icons.person_3_rounded),
-            color: Colors.white, // Set icon color to white
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => FavoriteActorsPage(),
-                ),
-              );
+        );
+      },
+    ),
+  ],
+  title: SizedBox(
+    width: 250, // Adjust this value as needed
+     child: Center(
+          child: GestureDetector(
+            onTap: () {
+              // Navigate to homepage when image is tapped
+              Navigator.pushNamed(context, '/homepage');
             },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(width: 50.0), // Adjust this value as needed
+          Image.asset(
+            'assets/images/image.png', // Change this to the path of your logo image
+            width: 150.0, // Adjust the width as needed
           ),
+          SizedBox(width: 0.0), // Adjust this value as needed
         ],
       ),
+    ),
+     ),
+  ),
+),
       body: Stack(
         children: [
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/first_page.jpg'), // Replace with your image asset
+                image: AssetImage('assets/images/first_page.jpg'),
                 fit: BoxFit.cover,
               ),
             ),
@@ -123,7 +132,12 @@ class _SearchPageState extends State<SearchPage> {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return Center(child: CircularProgressIndicator());
                       } else if (snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.white)));
+                        return Center(
+                          child: Text(
+                            'Error: ${snapshot.error}',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        );
                       } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                         return Center(
                           child: Text(
@@ -137,68 +151,95 @@ class _SearchPageState extends State<SearchPage> {
                           itemCount: movies.length,
                           itemBuilder: (context, index) {
                             final movie = movies[index];
-                            var description = movie.plot.length > 80 ? movie.plot.substring(0, 80) + "..." : movie.plot;
-                            return Container(
-                              margin: EdgeInsets.symmetric(vertical: 8.0),
-                              padding: EdgeInsets.all(6.0),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10.0),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.2),
-                                    blurRadius: 6.0,
-                                    offset: Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: ListTile(
-                                contentPadding: const EdgeInsets.all(5.0),
-                                leading: Container(
-                                  height: 100.0, // Changed height to 100.0
-                                  width: 70.0,   // Adjust the width as desired
-                                  decoration: BoxDecoration(
-                                    color: Colors.white, // Added this line
-                                    borderRadius: BorderRadius.circular(10.0), // Added this line
-                                  ),
-                                  child: FractionallySizedBox(
-                                    heightFactor: 1.0, // Full height of the container
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10.0), // Adjust this value to match the movie container
-                                      child: Image.asset(
-                                        movie.photoPath,
-                                        fit: BoxFit.cover,
+                            return FutureBuilder<bool>(
+                              future: _databaseHelper.checkPreferences(1, movie.id!),
+                              builder: (context, isFavSnapshot) {
+                                if (isFavSnapshot.connectionState == ConnectionState.waiting) {
+                                  return CircularProgressIndicator();
+                                } else if (isFavSnapshot.hasError) {
+                                  return Text('Error: ${isFavSnapshot.error}');
+                                } else {
+                                  bool isFav = isFavSnapshot.data ?? false;
+                                  var description = movie.plot.length > 80
+                                      ? movie.plot.substring(0, 80) + "..."
+                                      : movie.plot;
+                                  return Container(
+                                    margin: EdgeInsets.symmetric(vertical: 8.0),
+                                    padding: EdgeInsets.all(6.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.2),
+                                          blurRadius: 6.0,
+                                          offset: const Offset(0, 3),
+                                        ),
+                                      ],
+                                    ),
+                                    child: ListTile(
+                                      contentPadding: const EdgeInsets.all(5.0),
+                                      leading: Container(
+                                        height: 200.0,
+                                        width: 80.0,
+                                        decoration: BoxDecoration(
+                                          color: Color.fromARGB(255, 161, 38, 38),
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                        child: FractionallySizedBox(
+                                          heightFactor: 2.0,
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.circular(10.0),
+                                            child: Image.asset(
+                                              movie.photoPath,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                ),
-                                title: Text(
-                                  movie.title,
-                                  style: TextStyle(fontSize: 20, color: Colors.black), // Increase font size and change text color to black
-                                ),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      description,
-                                      style: const TextStyle(color: Color.fromRGBO(114, 114, 114, 1)), // Change text color to grey
-                                    ),
-                                    SizedBox(height: 4),
-                                    Text(
-                                      '${movie.year} | ${movie.duration}',
-                                      style: TextStyle(color: Color.fromRGBO(114, 114, 114, 1)), // Change text color to grey
-                                    ),
-                                  ],
-                                ),
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => MoviePage(movieId: movie.id!),
+                                      title: Text(
+                                        movie.title,
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      subtitle: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            description,
+                                            style: const TextStyle(
+                                              color: Color.fromRGBO(114, 114, 114, 1),
+                                            ),
+                                          ),
+                                          SizedBox(height: 4),
+                                          Text(
+                                            '${movie.year} | ${movie.duration}',
+                                            style: TextStyle(
+                                              color: Color.fromRGBO(114, 114, 114, 1),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      trailing: isFav
+                                          ? Icon(
+                                              Icons.favorite,
+                                              color: Colors.red,
+                                            )
+                                          : SizedBox(), // If not a favorite, show an empty SizedBox to maintain alignment
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => MoviePage(movieId: movie.id!),
+                                          ),
+                                        );
+                                      },
                                     ),
                                   );
-                                },
-                              ),
+                                }
+                              },
                             );
                           },
                         );
